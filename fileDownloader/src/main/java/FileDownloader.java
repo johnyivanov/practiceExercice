a15aeea688e31366636a329c07ae611729e3125d
Ivan Ivanov <ivanov.ivan.ivanovici@gmail.com>
2023-11-07 18:24:13 +0200

    Added a progressive status bar

--- fileDownloader/src/main/java/FileDownloader.java
+++ fileDownloader/src/main/java/FileDownloader.java
@@ -1,12 +1,12 @@
 import java.io.*;
-import java.net.URL;
 import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class FileDownloader {
 
     public static void main(String[] args) {
         if (args.length != 2) {
-            System.out.println("Usage: java FileDownloader <URL> <outputFile>");
+            System.out.println("Usage: java FileDownloaderWithProgressBar <URL> <outputFile>");
             System.exit(1);
         }
 
@@ -15,7 +15,7 @@ public class FileDownloader {
 
         try {
             downloadFile(fileURL, outputFile);
-            System.out.println("File downloaded successfully!");
+            System.out.println("\nFile downloaded successfully!");
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
         }
@@ -27,18 +27,42 @@ public class FileDownloader {
 
         int responseCode = connection.getResponseCode();
         if (responseCode == HttpURLConnection.HTTP_OK) {
+            long contentLength = connection.getContentLengthLong();
+            System.out.println("File size: " + contentLength + " bytes");
+
             try (InputStream inputStream = connection.getInputStream();
                  FileOutputStream outputStream = new FileOutputStream(outputFile)) {
 
                 byte[] buffer = new byte[1024];
                 int bytesRead;
+                int totalBytesRead = 0;
 
                 while ((bytesRead = inputStream.read(buffer)) != -1) {
                     outputStream.write(buffer, 0, bytesRead);
+                    totalBytesRead += bytesRead;
+                    progressPercentage(totalBytesRead,contentLength);
                 }
+
+
             }
         } else {
             throw new IOException("HTTP Error: " + responseCode);
         }
     }
+
+    public static void progressPercentage(int bytesRead, long contentLength) {
+        int progress = (int) (bytesRead * 1.0 / contentLength * 100);
+        System.out.print("\r[");
+
+        for (int i = 0; i < 50; i++) {
+            if (i < progress / 2) {
+                System.out.print("=");
+            } else {
+                System.out.print(" ");
+            }
+        }
+        System.out.print("] " + progress + "%");
+
+    }
+
 }
